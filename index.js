a2194a62d0f9b3a193f857977cbe149f883f479d
Md Azim <mdazim.cse@gmail.com>
2021-06-10 02:54:01 +0600

    added database

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+
+
 const port = process.env.PORT || 5055
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwkyk.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World with azim!')
+  res.send('Hello World with azim !')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productsCollection = client.db("guitarBazar").collection("products");
+  
+
+  app.get('/products', (req, res) => {
+    productsCollection.find()
+    .toArray((err, items) => {
+      res.send(items)
+      console.log('from database', items);
+    })
+  })
+
+
+  app.post('/addData', (req, res) => {
+    const newData = req.body;
+    console.log('adding new Data', newData);
+    productsCollection.insertOne(newData)
+    .then(result => {
+      console.log('inserted count', result.insertedCount)
+      res.send(result.insertedCount > 0)
+    })
+
+  })
+
+  // client.close();
+});
+
+
+
+
+
+
+app.listen(port)
